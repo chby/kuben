87b50bb6cf6b023f3cf1604916cbf360b6cb5ab0
Erik Tehler <erik@tehler.se>
2015-09-11 10:41:26 +0200

    fix

--- notify-kuben/index.php
+++ notify-kuben/index.php
@@ -3,16 +3,15 @@
 	
 	$tomorrow = date("Y-m-d", strtotime("+1 day", time()));
 
-	$booked_duty_days = array();
+	wp_mail("erik@tehler.se", "[Kubens föräldrawebb] Imorgon", $tomorrow);
 	
-	$user_booked_duty_days = array();
 	$users = get_users(array('meta_key' => 'booked_duty_days'));
 	
 	foreach ($users as $user) { 
 		$data = get_userdata($user->ID);
 		$meta = get_user_meta($user->ID, 'booked_duty_days', true);
 		if (in_array($tomorrow, $meta)) {
-			wp_mail($user->user_email, "[Kubens föräldrawebb] Du är bokad för jour imorgon", "Du är bokad för jour imorgon (".$tomorrow.").\n\n");
+			wp_mail($user->user_email, "[Kubens föräldrawebb] Du är bokad för jour imorgon", "Du är bokad för jour imorgon (".$tomorrow.").");
 		}
 	} 	
 	
